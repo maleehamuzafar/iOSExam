43d35ccc48b040d8f23cc500009975ec8c70a676
Maleeha Muzafar <maleeha.muzafar@gmail.com>
2025-03-05 10:38:46 +0300

    Initial Commit

---
+++ iOSExam/iOSExamApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSExamApp.swift
+//  iOSExam
+//
+//  Created by Maleeha Muzafar on 05/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct iOSExamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
