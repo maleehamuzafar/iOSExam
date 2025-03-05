ad45c03049bde558d49e133c312500a1149efb0a
Maleeha Muzafar <maleeha.muzafar@gmail.com>
2025-03-05 10:48:26 +0300

    Initial commit with models `Category` and `Item`

---
+++ iOSExam/Models/Category.swift
@@ -0,0 +1,20 @@
+//
+//  Category.swift
+//  iOSExam
+//
+//  Created by Maleeha Muzafar on 05/03/2025.
+//
+
+import Foundation
+
+struct Category {
+    let id: UUID
+    let items: [Item]
+    let imageURL: URL
+    
+    init(id: UUID = UUID(), items: [Item], imageURL: URL) {
+        self.id = id
+        self.items = items
+        self.imageURL = imageURL
+    }
+}
---
+++ iOSExam/Models/Item.swift
@@ -0,0 +1,20 @@
+//
+//  Item.swift
+//  iOSExam
+//
+//  Created by Maleeha Muzafar on 05/03/2025.
+//
+
+import Foundation
+
+struct Item {
+    let id: UUID
+    let name: String
+    let imageURL: URL
+    
+    init(id: UUID = UUID(), name: String, imageURL: URL) {
+        self.id = id
+        self.name = name
+        self.imageURL = imageURL
+    }
+}
